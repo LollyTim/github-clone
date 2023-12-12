6761f4ef91ec501521ee69e3802edc817437a60b
Ololade Sanya <ololadetimileyin3@gmail.com>
2023-12-12 22:48:36 +0100

    updated the  js  file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
  let name = "lolly"
 
-const money = () => {
-    if (name === "lolly"){
-        console.log( `this user name is lolly ${name}`)
-    }
+const Salutation = () => {
+    return name;
 }
